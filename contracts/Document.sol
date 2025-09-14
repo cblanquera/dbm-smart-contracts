5018591b3619d4fef1822c857364854f71f93eec
Chris Blanquera <chris@incept.asia>
2025-09-14 18:19:14 +0800

    adding more minting options

--- contracts/Document.sol
+++ contracts/Document.sol
@@ -3,6 +3,8 @@
 pragma solidity ^0.8.29;
 
 import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import { MessageHashUtils } from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 import { IERC721Mintable } from "./IERC721Mintable.sol";
 import { 
@@ -53,6 +55,65 @@ contract Document is
     _safeMint(recipient, tokenId);
     // Map the token metadata
     _mapData(tokenId, IERC721TokenMetadata(_msgSender()));
+    return tokenId;
+  }
+
+  /**
+   * @dev Allows anyone to mint tokens that was approved by the owner.
+   * (ie. moves the burden of gas to the minter)
+   */
+  function mint(
+    IERC721TokenMetadata metadata, 
+    address recipient, 
+    bytes memory proof
+  ) 
+    external nonReentrant 
+  {
+    // Note: We are doing it this way instead of creating a variable...
+    //4. Make sure the minter signed this off
+    if (!hasRole(
+      _MINTER_ROLE, 
+      // 3. Then recover the signer (address)
+      ECDSA.recover(
+        // 2. Then convert to bytes32
+        MessageHashUtils.toEthSignedMessageHash(
+          // 1. Make a message hash
+          keccak256(
+            abi.encodePacked(
+              "mint", 
+              recipient, 
+              address(metadata)
+            )
+          )
+        ),
+        proof
+      )
+    )) {
+      revert InvalidProof();
+    }
+
+    // Get the next token ID
+    uint256 tokenId = super.totalSupply() + 1;
+    // Mint the token
+    _safeMint(recipient, tokenId);
+    // Map the token metadata
+    _mapData(tokenId, metadata);
+  }
+
+  /**
+   * @dev Allows the minter role to mint to `recipient` with 
+   * specific `data`. This is used in the case platforms want to 
+   * facilitate the mint (ie. to save on gas).
+   */
+  function mint(IERC721TokenMetadata data, address recipient) 
+    external onlyRole(_MINTER_ROLE) nonReentrant returns(uint256) 
+  {
+    // Get the next token ID
+    uint256 tokenId = super.totalSupply() + 1;
+    // Mint the token
+    _safeMint(recipient, tokenId);
+    // Map the token metadata
+    _mapData(tokenId, data);
     return tokenId;
   }
 }
--- contracts/IERC721Mintable.sol
+++ contracts/IERC721Mintable.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.29;
 
+import { IERC721TokenMetadata } from "./IERC721TokenMetadata.sol";
+
 /**
  * @dev Interface for minting. This is used by a document spec
  * (ie. NCA, SARO) in order to delegate minting to the main contract.
@@ -11,4 +13,22 @@ interface IERC721Mintable {
    * @dev Allows ITokenMetadata to mint to the `recipient`. 
    */
   function mint(address recipient) external returns(uint256);
+
+  /**
+   * @dev Allows anyone to mint tokens that was approved by the owner.
+   * (ie. moves the burden of gas to the minter)
+   */
+  function mint(
+    IERC721TokenMetadata metadata, 
+    address recipient, 
+    bytes memory proof
+  ) external;
+
+  /**
+   * @dev Allows the minter role to mint to `recipient` with 
+   * specific `data`. This is used in the case platforms want to 
+   * facilitate the mint (ie. to save on gas).
+   */
+  function mint(IERC721TokenMetadata data, address recipient) 
+    external returns(uint256);
 }
